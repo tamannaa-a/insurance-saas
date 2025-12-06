1186c630f738aa848831fb39c94a7cb5990bfd66
Tamanna Vaikkath <vaikkathtamanna@gmail.com>
2025-12-06 20:30:05 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: "http://localhost:8000"
 });
 
-// Helper to set token per request
 export const authHeader = (token) => ({
   headers: {
     Authorization: `Bearer ${token}`
